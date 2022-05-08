f2c5895eb1b5fae21b4223fe649032498742af14
Ahmed <a.tamer87@yahoo.com>
2022-05-08 06:46:00 +0200

    Navbar Added

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primaryBlue: "#00d8ff",
+        primaryPurple: "#6a00ff"
+      }
+    },
+  },
+  plugins: [],
+}
